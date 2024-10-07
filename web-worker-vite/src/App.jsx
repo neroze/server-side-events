d0297802400b3ca873c36b426945af7893c5341f
Niraj Maharjan <nmh@logpoint.com>
2024-10-07 14:24:20 +0545

    feat: vite app template update

--- web-worker-vite/src/App.jsx
+++ web-worker-vite/src/App.jsx
@@ -2,7 +2,7 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function fetchPersonWithWorker(id) {
   return new Promise((resolve, reject) => {
@@ -23,6 +23,7 @@ function fetchPersonWithWorker(id) {
 }
 
 const App = () => {
+  const [users, setUsers] = useState({})
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -30,6 +31,7 @@ const App = () => {
         const [luke, leia] = await Promise.all([fetchPersonWithWorker(1), fetchPersonWithWorker(5)]);
         console.log("Luke:", luke);
         console.log("Leia:", leia);
+        setUsers({luke, leia})
       } catch (error) {
         console.error("Error fetching characters:", error);
       }
@@ -38,42 +40,30 @@ const App = () => {
     fetchData();
   }, []);
 
+  if (! users.leia) {
+    return <div>Loading...</div>
+  }
+
   return (
     <div className="App">
       <h1>Star Wars Character Fetcher with Web Workers</h1>
-      <p>Open the console to see the results.</p>
+      { users?.luke &&
+          <div>
+          <h3>Character Name: {users.luke.name}</h3>
+          <p>Height: {users.luke.height}</p>
+          <p>Mass: {users.luke.mass}</p>
+        </div>
+      }
+
+{ users?.leia &&
+          <div>
+          <h3>Character Name: {users.leia.name}</h3>
+          <p>Height: {users.leia.height}</p>
+          <p>Mass: {users.leia.mass}</p>
+        </div>
+      }
     </div>
   );
 };
-
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
 
 export default App
